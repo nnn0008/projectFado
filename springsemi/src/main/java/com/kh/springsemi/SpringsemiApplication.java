5cc2435b8cf4e768c32c7c8606a277082097f2d5
nnn0008 <secondepect@gmail.com>
2023-09-18 00:05:24 +0900

    첫 업로드

---
+++ springsemi/src/main/java/com/kh/springsemi/SpringsemiApplication.java
@@ -0,0 +1,13 @@
+package com.kh.springsemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsemiApplication.class, args);
+	}
+
+}
---
+++ springsemi/src/test/java/com/kh/springsemi/SpringsemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.springsemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

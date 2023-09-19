4fa2106f29a2daced6258b2b0d2b536187a30b07
newstar95 <byeol12120@gmail.com>
2023-09-19 14:20:56 +0900

    회원가입 구현 중

--- springsemi/src/main/java/com/kh/springsemi/controller/MemberController.java
+++ springsemi/src/main/java/com/kh/springsemi/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.kh.springsemi.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,7 +42,27 @@ public class MemberController {
 		return "/WEB-INF/views/member/login.jsp";
 	}
 	
-//	@PostMapping("/login")
-	
-	
+	@PostMapping("/login")
+	public String login(@ModelAttribute MemberDto inputDto, HttpSession session) {
+		MemberDto findDto = memberDao.selectOne(inputDto.getMemberId());
+		
+		if(findDto == null) { 
+			return "redirect:login?error";
+		}
+		
+		boolean isCorrectPw = inputDto.getMemberPw().equals(findDto.getMemberPw());
+		if(isCorrectPw) {
+			//차단된 회원이라면 작업 중지 후 오류 발생
+//			MemberBlockDto blockDto = memberDao.selectBlockOne(findDto.getMemberId()); //차단된 회원
+//			if (blockDto != null) {
+//				//return "redirect:오류페이지";
+//				throw new AuthorityException("차단된 회원");
+//			}
+			session.setAttribute("name", findDto.getMemberId()); 
+			return "redirect:/";
+		}
+		else {
+			return "redirect:login?error";
+		}
+	}
 }
--- springsemi/src/main/webapp/WEB-INF/views/member/join.jsp
+++ springsemi/src/main/webapp/WEB-INF/views/member/join.jsp
@@ -3,9 +3,31 @@
     
 <jsp:include page="/WEB-INF/views/template/header.jsp"></jsp:include>
 
+<form>
+	<label>아이디</label>
+	<input type="text" name="memberId" placeholder="영문 소문자, 숫자 5~20자">
+	
+	<label>비밀번호</label>
+	<input type="password" name="memberPw" placeholder="영문,숫자,특수문자 반드시 1개 이상 포함">
+	
+	<label>비밀번호 확인</label>
+	<input type="password" placeholder="비밀번호 한 번 더 입력">
+	
+	<label>닉네임</label>
+	<input type="text" name="memberNickname"  placeholder="한글 또는 숫자 2~10자">
+		
+	<label>생년월일</label>
+    <input type="date" name="memberBirth">
+    
+    <label>연락처</label>
+    <input type="tel" name="memberContact" placeholder="010XXXXXXXX (- 없이)">
+    
+	<label>이메일</label>
+    <input type="text" name="memberEmail" placeholder="fado@gmail.com">
 
-
-
-
+	<button type="submit">
+	가입하기
+	</button>    
+</form>
 
 <jsp:include page="/WEB-INF/views/template/footer.jsp"></jsp:include>

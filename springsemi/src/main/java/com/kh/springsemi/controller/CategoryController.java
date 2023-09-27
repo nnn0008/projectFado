f9bd4f42b8c75c5776dc30e24f881b503aa9e73e
nnn0008 <secondepect@gmail.com>
2023-09-27 18:10:32 +0900

    관리자 기능 중 카테고리 대분류 / 소분류 만들기 (진행 중)

---
+++ springsemi/src/main/java/com/kh/springsemi/controller/CategoryController.java
@@ -0,0 +1,50 @@
+package com.kh.springsemi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.springsemi.dao.MajorCategoryDao;
+import com.kh.springsemi.dao.MinorCategoryDao;
+import com.kh.springsemi.dto.MajorCategoryDto;
+import com.kh.springsemi.dto.MinorCategoryDto;
+
+@Controller
+@RequestMapping("/admin/category")
+public class CategoryController {
+	@Autowired
+	private MajorCategoryDao majorCategoryDao;
+	
+	@Autowired
+	private MinorCategoryDao minorCategoryDao;
+	
+	@GetMapping("/list")
+	public String write(Model majorModel, Model minorModel) {
+		List<MajorCategoryDto> majorList = majorCategoryDao.selectList();
+		majorModel.addAttribute("majorList", majorList);
+		List<MinorCategoryDto> minorList = minorCategoryDao.selectList();
+		return "/WEB-INF/views/admin/category/list.jsp";
+	}
+	
+	@PostMapping("/list")
+	public String write(@ModelAttribute MajorCategoryDto majorCategoryDto, @ModelAttribute MinorCategoryDto minorCategoryDto) {
+		int majorCategoryNo = majorCategoryDao.sequence();
+		int minorCategoryNo = minorCategoryDao.sequence();
+		majorCategoryDto.setMajorCategoryNo(majorCategoryNo);
+		minorCategoryDto.setMajorCategoryNo(majorCategoryNo);
+		minorCategoryDto.setMinorCategoryNo(minorCategoryNo);
+		
+		majorCategoryDao.insert(majorCategoryDto);
+		minorCategoryDao.insert(minorCategoryDto);
+		
+		return "/WEB-INF/views/admin/category/list.jsp";
+	}
+	
+	
+}
